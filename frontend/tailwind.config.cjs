9ecdbf42c24d0e45a440a905d10ae651363704d1
marcosefritose <m.gehrke97@t-online.de>
2022-11-28 16:17:35 +0100

    WIP interactive map tryout

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gag-primary': '#d19000',
+        'gag-light': '#ffff7f'
+      }
+    },
   },
+  safelist: ['stroke-gag-primary', 'fill-gag-primary'],
   plugins: [],
 }
