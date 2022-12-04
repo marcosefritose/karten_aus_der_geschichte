b39e1a4851aa8aa0d3a88e8d199b2763cd9d2859
marcosefritose <m.gehrke97@t-online.de>
2022-12-04 21:48:51 +0100

    zoomable map and feature highlighting on interaction

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,104 +1,195 @@
 <script>
 	import { onMount } from 'svelte';
-	import { json, geoPath, geoNaturalEarth1, geoMercator } from 'd3';
+	import { json, geoPath, zoom, zoomIdentity, pointer, select, geoNaturalEarth1, geoMercator } from 'd3';
+	import { identity, object_without_properties } from 'svelte/internal';
 
-    const  projection  =  geoNaturalEarth1();
-    const  path  =  geoPath(projection);
+	const projection = geoNaturalEarth1();
+	const path = geoPath(projection);
 	export let data;
 	let episodes = [];
-    let dataset = [];
-    let markers = [];
-    let selected;
+	let dataset = [];
+	let markers = [];
+	let selected;
 
-    // Static url until images are served from flask server
-    let imgUrl = "https://images.podigee-cdn.net/0x,sw_d9izO9QfY97alR2f13b7IrOVeXM5gFZOjetk-5sTg=/https://cdn.podigee.com/uploads/u32412/b610d282-8f99-4604-a16f-28ada94ab76a.jpg";
+	let bindHandleZoom, bindInitZoom;
 
-    function selectEpisode(id) {
-		if(id === selected) {
+	// Static url until images are served from flask server
+	let imgUrl =
+		'https://images.podigee-cdn.net/0x,sw_d9izO9QfY97alR2f13b7IrOVeXM5gFZOjetk-5sTg=/https://cdn.podigee.com/uploads/u32412/b610d282-8f99-4604-a16f-28ada94ab76a.jpg';
+
+	function selectEpisode(id) {
+		if (id === selected) {
 			selected = null;
 		} else {
 			selected = id;
 		}
 
+		let coordinates = markers.filter(loc => {
+			return loc.id === id;
+		})
+
+		if(coordinates.length !== 0) {
+			let geoFeature = getGeoFeatureForEpisode(id, coordinates);
+			clicked(geoFeature)
+		}
+		
 		let listEntry = document.getElementById(id);
-		listEntry.scrollIntoView()
-    }
+		listEntry.scrollIntoView({
+			behavior: 'smooth'
+		});
+	}
+
+	$: zoomX = zoom().scaleExtent([1, 5]).on('zoom', handleZoom);
+
+	function handleZoom(e) {
+		select(bindHandleZoom).attr('transform', e.transform);
+	}
+
+	$: if (bindInitZoom) {
+		select(bindInitZoom).call(zoomX);
+	}
+
+	function clicked(d) {
+		const [[x0, y0], [x1, y1]] = path.bounds(d);
+
+		select(bindHandleZoom).transition()
+			.duration(750)
+			.call(
+				zoomX.transform,
+				zoomIdentity
+					.translate(1000 / 2, 500 / 2)
+					.scale(Math.min(3, 0.9 / Math.max((x1 - x0) / 1720, (y1 - y0) / 1267)))
+					.translate(-(x0 + x1) / 2, -(y0 + y1) / 2)
+			);
+	}
+
+	function getGeoFeatureForEpisode(id, coordinates) {
+		coordinates = coordinates.map(coord => {
+			return [parseFloat(coord.longitude), parseFloat(coord.latitude)];
+		});
+
+		if(coordinates.length === 1) {
+			coordinates = [coordinates[0], coordinates[0]]
+		}
+		return {
+			type: 'Feature',
+			geometry: {
+				type: "Polygon",
+				coordinates: [coordinates]
+			},
+			id: id
+		}
+	}
 
 	onMount(() => {
 		episodes = data.episodes;
-        episodes.forEach(ep => {
-            let locations = ep.locations.map((loc) => {
-                return {id: ep.id, longitude: loc.longitude, latitude: loc.latitude}
-            });           
-            markers = markers.concat(locations)
-        })
+		episodes.forEach((ep) => {
+			let locations = ep.locations.map((loc) => {
+				return { id: ep.id, longitude: loc.longitude, latitude: loc.latitude };
+			});
+			markers = markers.concat(locations);
+		});
 
-        // ToDo move geojson to flask server/static files
+		// ToDo move geojson to flask server/static files
 		json(
 			'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson'
 		).then((data) => {
 			dataset = data.features;
 		});
-
-        console.log(episodes[0]);
 	});
 </script>
 
 <main class="">
-	<div class="flex-col w-screen h-screen bg-gray-50 place-items-center overflow-clip">
-		<h1 class="flex-auto text-center text-3xl font-bold py-3">Karten aus der Geschichte</h1>
+	<div class="flex-col w-screen h-screen bg-blue-100 place-items-center overflow-clip">
 		<div class="flex-auto w-full h-full overflow-hidden">
 			<!-- World Map -->
-			<svg class="w-full h-full" viewBox="0 0 1000 550" preserveAspectRatio="xMidYMin slice">
-				{#each dataset as data}
-					<path class="fill-gray-300 stroke-1" d={path(data)} />
-				{/each}
-				{#each markers as {id, longitude, latitude}}
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<line
-						on:click={selectEpisode(id)}
-						class="{selected === id ? 'stroke-5 stroke-gag-primary' : 'stroke-2 stroke-gray-600'} hover:stroke-4" stroke-linecap="round"
-						x1="{projection([longitude, latitude])[0]}"
-						x2="{projection([longitude, latitude])[0]+0.1}"
-						y1="{projection([longitude, latitude])[1]}"
-						y2="{projection([longitude, latitude])[1]+0.1}" 
-					/>
-				{/each}
+			<svg
+				id="world-map"
+				class="w-full h-full bg-slate-600"
+				viewBox="0 0 1000 500"
+				preserveAspectRatio="xMidYMid slice"
+				bind:this={bindInitZoom}
+			>
+				<g class="countries" bind:this={bindHandleZoom}>
+					{#each dataset as data}
+						<!-- svelte-ignore a11y-click-events-have-key-events -->
+						<path
+							id={data.id}
+							class="fill-gray-300 stroke-1"
+							d={path(data)}
+						/>
+					{/each}
+					{#each markers as { id, longitude, latitude }}
+						<!-- svelte-ignore a11y-click-events-have-key-events -->
+						<line
+							on:click={selectEpisode(id)}
+							class="{selected === id
+								? 'stroke-5 stroke-gag-primary'
+								: 'stroke-2 stroke-gray-600'} hover:stroke-4"
+							stroke-linecap="round"
+							x1={projection([longitude, latitude])[0]}
+							x2={projection([longitude, latitude])[0] + 0.1}
+							y1={projection([longitude, latitude])[1]}
+							y2={projection([longitude, latitude])[1] + 0.1}
+						/>
+					{/each}
+				</g>
 			</svg>
+			<!-- Logo -->
+			<div
+				class="h-14 lg:h-20 absolute top-0 flex bg-slate-400 bg-opacity-80 border-dashed border-gag-primary border-b border-r rounded-br-md"
+			>
+				<img src="/gag_logo.png" class="object-contain" alt="" />
+			</div>
 			<!-- Episode List -->
-			<div id="list" class="flex items-center absolute right-0 bottom-5 lg:top-0 w-full lg:w-1/3 xl:w-1/4 h-1/2 lg:h-full">
+			<div
+				id="list"
+				class="flex items-center absolute right-0 bottom-0 lg:top-0 w-full lg:w-1/3 xl:w-1/4 h-1/2 lg:h-full"
+			>
 				<div
 					class="bg-gray-200 opacity-80 overflow-y-scroll h-5/6 m-3 p-3  scrollbar-thin rounded-xl scrollbar-thumb-gag-primary scrollbar-track-gray-400"
 				>
 					{#each episodes as episode (episode.id)}
-						<div id="{episode.id}"class="flex gap-2 border py-1">
+						<div id={episode.id} class="flex gap-2 border py-1">
 							<!-- svelte-ignore a11y-click-events-have-key-events -->
-							<img src={imgUrl} class="w-16 h-16 rounded-sm border border-gray-400 hover:cursor-pointer" alt="Thumbnail Picture Episode {episode.id}" on:click={selectEpisode(episode.id)} />
+							<img
+								src={imgUrl}
+								class="w-16 h-16 rounded-sm border border-gray-400 hover:cursor-pointer"
+								alt="Thumbnail Picture Episode {episode.id}"
+								on:click={selectEpisode(episode.id)}
+							/>
 							<div class="flex-row">
 								<!-- svelte-ignore a11y-click-events-have-key-events -->
-								<span class="bg-gag-primary text-white px-2 py-1 rounded-md text-sm font-bold hover:cursor-pointer" on:click={selectEpisode(episode.id)}
-                                >{episode.id}</span
+								<span
+									class="bg-gag-primary text-white px-2 py-1 rounded-md text-sm font-bold hover:cursor-pointer"
+									on:click={selectEpisode(episode.id)}>{episode.id}</span
 								>
-                                
+
 								<!-- svelte-ignore a11y-click-events-have-key-events -->
 								<div class="w-full">
-									<span class="mb-1 inline-block text-small font-semibold hover:cursor-pointer" on:click={selectEpisode(episode.id)}>{episode.title.split(': ')[1]}</span
+									<span
+										class="mb-1 inline-block text-small font-semibold hover:cursor-pointer"
+										on:click={selectEpisode(episode.id)}>{episode.title.split(': ')[1]}</span
 									><br />
 								</div>
 							</div>
 						</div>
-                        <div class="{selected === episode.id ? '' : 'hidden'} px-3">
-                            <div class="location-tabs flex flex-wrap gap-2 text-sm">
+						<div class="{selected === episode.id ? '' : 'hidden'} px-3">
+							<div class="location-tabs flex flex-wrap gap-2 text-sm">
 								{#each episode.locations as loc}
-								<span class="py-1 px-2 bg-white border border-gag-primary text-gag-primary rounded-lg">{loc.name}</span>
-                                {/each}
-                            </div>
-							<span class="py-1 text-xs font-semibold">Veröffentlicht am {new Date(episode.published).toLocaleDateString('de-DE')}</span>
-                            <p class="text-small">
-                                {episode.summary}
-                            </p>
-
-                        </div>
+									<span
+										class="py-1 px-2 bg-white border border-gag-primary text-gag-primary rounded-lg"
+										>{loc.name}</span
+									>
+								{/each}
+							</div>
+							<span class="py-1 text-xs font-semibold"
+								>Veröffentlicht am {new Date(episode.published).toLocaleDateString('de-DE')}</span
+							>
+							<p class="text-small">
+								{episode.summary}
+							</p>
+						</div>
 					{/each}
 				</div>
 			</div>
