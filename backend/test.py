95eff9ae2f6e72477251aa517e9c2f47549199ba
marcosefritose <m.gehrke97@t-online.de>
2022-12-31 01:49:10 +0100

    cleaned up backend

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-#response = requests.get(BASE + "episodes")
-# response = requests.put(BASE + "episodes/1372", {'title': 'bischt deppert?'})
-# response = requests.get(BASE + "episode/GAG374")
-response = requests.get(BASE + "episodes/")
-print(response.json())
