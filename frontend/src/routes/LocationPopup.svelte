d0a4f0a106a71d151a5c6a7a2cbec31d0aebdad4
marcosefritose <m.gehrke97@t-online.de>
2023-01-04 11:41:43 +0100

    added content and styling to location popup

--- frontend/src/routes/LocationPopup.svelte
+++ frontend/src/routes/LocationPopup.svelte
@@ -6,6 +6,11 @@
 </script>
 
 
-<div class="absolute w-32 h-32 bg-green-300" style="left: {x}px; top: {y}px">
-    {location}
+<div class="absolute bg-gray-300 max-w-sm rounded-md border border-gray-900" style="left: {x}px; top: {y}px">
+    <p class="text-center text-lg font-semibold py-1">{location.name}</p>
+    <ul class="p-1">
+        {#each location.episodes as episode, id}
+            <li class="pb-1"><span class="font-bold">{episode.id}</span>: {episode.title}</li>
+        {/each}
+    </ul>
 </div>
