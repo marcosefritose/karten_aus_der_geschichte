99e5e9c04a2d6f07eaf0469c9cdf3d4f6ebe0359
marcosefritose <m.gehrke97@t-online.de>
2023-01-08 17:50:37 +0100

    added personal website link

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,6 +15,11 @@
 			<img src="/gag_logo.png" class="object-contain" alt="" />
 		</div>
 
+		<!-- Link -->
+		<div class="absolute right-0 top-0 p-2 text-sm">
+			Made with 🧠 by <a href="https://marcose.eu" target="_blank" rel="noopener noreferrer" class="underline underline-offset-2">marcose</a>.
+		</div>
+
 		<!-- Episode List -->
 		<EpisodeList/>
 	</div>
