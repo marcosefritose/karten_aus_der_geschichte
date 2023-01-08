bf73504a5cf278fc7436c4ea4679550bcf7afac1
marcosefritose <m.gehrke97@t-online.de>
2023-01-08 20:44:44 +0100

    docker-compose & env configurations

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,11 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
+		adapter: adapter(),
 	},
 	preprocess: [
 		preprocess({
