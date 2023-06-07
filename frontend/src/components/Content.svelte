30526048f52d32acf361b9e1e3f602cf600eb7ce
marcosefritose <m.gehrke97@t-online.de>
2023-06-07 17:17:43 +0200

    style(fe): Redesigned side content navigation

--- frontend/src/components/Content.svelte
+++ frontend/src/components/Content.svelte
@@ -2,7 +2,7 @@
   import EpisodeList from '../components/EpisodeList.svelte';
   import Locations from '../components/Locations.svelte';
   import Search from '../components/Search.svelte';
-  import { setShowHistoricMaps, selectedEpisode, showHistoricMap } from '../routes/store';
+  import { selectedTime, showHistoricMap } from '../routes/store';
   import Timeline from '../components/Timeline.svelte';
 
   let popupSelectorChecked = false;
@@ -25,89 +25,73 @@
         ? `transform: translateY(${contentBodyHeight}px)`
         : `transform: translateX(${contentBodyWidth}px)`
       : ''}
-    class="pointer-events-none my-3 flex h-full w-full flex-col opacity-80 transition md:h-5/6"
+    class="flex h-full w-full flex-col opacity-80 transition md:h-5/6 md:flex-row"
   >
-    <div class="relative select-none px-3">
-      <div class="flex justify-between">
-        <div
-          style={windowWidth > 768 ? `transform: translate(-135%, 101%) rotate(-90deg);` : ''}
-          class="visible cursor-pointer rounded-t-md border border-b-0 border-gray-800 bg-gray-400"
-        >
-          <img
-            class="pointer-events-auto h-8 w-8"
-            style={hideContent ? 'transform: rotate(180deg)' : ''}
-            src="icons/double-arrow-down.svg"
-            alt="Double Arrow Icon"
-            on:click={() => (hideContent = !hideContent)}
-          />
+    <div
+      class="pointer-events-auto flex h-8 w-fit flex-row justify-start gap-3 bg-zinc-400 md:h-fit md:w-8 md:flex-col"
+    >
+      <div>
+        <img
+          class="h-8 w-8 cursor-pointer {selectedContent == 'list' ? 'bg-zinc-200' : 'bg-zinc-400'}"
+          src="icons/list.svg"
+          alt="Episode List Icon"
+          on:click={() => (selectedContent = 'list')}
+          on:keydown={() => (selectedContent = 'list')}
+        />
+      </div>
+      <img
+        class="h-8 w-8 cursor-pointer p-1 {selectedContent == 'timeline'
+          ? 'bg-zinc-200'
+          : 'bg-zinc-400'}"
+        src="icons/time-past.svg"
+        alt="Episode List Icon"
+        on:click={() => (selectedContent = 'timeline')}
+        on:keydown={() => (selectedContent = 'timeline')}
+      />
+      <img
+        class="h-8 w-8 cursor-pointer p-1 {selectedContent == 'locations'
+          ? 'bg-zinc-200'
+          : 'bg-zinc-400'}"
+        src="icons/pinpoint.svg"
+        alt="Location Pinpoint Icon"
+        on:click={() => (selectedContent = 'locations')}
+        on:keydown={() => (selectedContent = 'locations')}
+      />
+      <label for="popup-selector" class="cursor-pointer border-x md:border-x-0 md:border-y">
+        <input
+          type="checkbox"
+          id="popup-selector"
+          class="peer sr-only"
+          bind:checked={$showHistoricMap}
+        />
+        <div class="peer-checked:bg-gag-primary flex items-center px-1 md:h-fit md:w-8 md:flex-col">
+          <img src="icons/map.svg" alt="Episode List Icon" class="h-8 w-8" />
+          <span class="text-center text-xs font-bold">{$selectedTime.year}</span>
         </div>
-        <div class="pointer-events-auto flex items-center justify-end gap-2">
-          <label
-            for="popup-selector"
-            class="flex cursor-pointer gap-1 self-start rounded-full border border-gray-800 bg-gray-300"
-          >
-            <input
-              type="checkbox"
-              id="popup-selector"
-              class="peer sr-only"
-              bind:checked={$showHistoricMap}
-            />
-            <span class="bg-gag-primary flex h-7 w-7 rounded-full p-1 peer-checked:bg-gray-300">
-              <img src="icons/location-pin.svg" alt="Episode List Icon" />
-            </span>
-            <span class="peer-checked:bg-gag-primary flex h-7 w-7 rounded-full bg-gray-300 p-1">
-              <img src="icons/map.svg" alt="Episode List Icon" />
-            </span>
-          </label>
-          <img
-            class="h-8 w-8 cursor-pointer rounded-t-md border border-b-0 border-gray-800 {selectedContent ==
-            'list'
-              ? 'bg-gag-primary'
-              : 'bg-gray-300'}"
-            src="icons/list.svg"
-            alt="Episode List Icon"
-            on:click={() => (selectedContent = 'list')}
-            on:keydown={() => (selectedContent = 'list')}
-          />
-          <img
-            class="h-8 w-8 cursor-pointer rounded-t-md border border-b-0 border-gray-800 p-1 {selectedContent ==
-            'timeline'
-              ? 'bg-gag-primary'
-              : 'bg-gray-300'}"
-            src="icons/time-past.svg"
-            alt="Episode List Icon"
-            on:click={() => (selectedContent = 'timeline')}
-            on:keydown={() => (selectedContent = 'timeline')}
-          />
-          <img
-            class="h-8 w-8 cursor-pointer rounded-t-md border border-b-0 border-gray-800 p-1 {selectedContent ==
-            'locations'
-              ? 'bg-gag-primary'
-              : 'bg-gray-300'}"
-            src="icons/pinpoint.svg"
-            alt="Location Pinpoint Icon"
-            on:click={() => (selectedContent = 'locations')}
-            on:keydown={() => (selectedContent = 'locations')}
-          />
-          <img
-            class="h-8 w-8 cursor-pointer rounded-t-md border border-b-0 border-gray-800 {selectedContent ==
-            'search'
-              ? 'bg-gag-primary'
-              : 'bg-gray-300'}"
-            src="icons/search.svg"
-            alt="Search Icon"
-            on:click={() => (selectedContent = 'search')}
-            on:keydown={() => (selectedContent = 'search')}
-          />
-        </div>
+      </label>
+
+      <div
+        class="h-8 w-8 cursor-pointer bg-gray-400"
+        style={windowWidth > 768 ? `transform: rotate(-90deg);` : ''}
+      >
+        <img
+          class="h-8 w-8"
+          style={hideContent ? 'transform: rotate(180deg)' : ''}
+          src="icons/double-arrow-down.svg"
+          alt="Double Arrow Icon"
+          on:click={() => {
+            hideContent = !hideContent;
+            console.log(contentBodyWidth);
+          }}
+        />
       </div>
     </div>
 
     <div
-      bind:clientHeight={contentBodyHeight}
-      bind:clientWidth={contentBodyWidth}
+      bind:offsetHeight={contentBodyHeight}
+      bind:offsetWidth={contentBodyWidth}
       id="content-body"
-      class="scrollbar-thin scrollbar-track-gray-400 scrollbar-thumb-gag-primary pointer-events-auto overflow-y-scroll border-t border-gray-800 bg-gray-200 p-2"
+      class="scrollbar-thin scrollbar-track-gray-400 scrollbar-thumb-gag-primary pointer-events-auto w-full overflow-y-scroll bg-zinc-200 p-2"
     >
       {#if selectedContent == 'list'}
         <EpisodeList />
